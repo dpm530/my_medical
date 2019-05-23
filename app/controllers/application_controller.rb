c0ee59d24073c6857035ef2a41d502bcbfda5134
Daniel Palacios <danielpalacios530@gmail.com>
2019-05-23 18:20:47 -0500

    added user account functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+   def index
+   end
+
+   private
+
+      def after_sign_in_path_for(resource)
+         users_path
+      end
+
+      def after_sign_out_path_for(resource)
+         root_path
+      end
+
+      def after_sign_up_path_for(resource)
+         users_path
+      end
+
+
 end
